c1584d2bcca54262eeebe0ec5953cdb825e39e4b
ShirayukiRin <ShirashimiLin@gmail.com>
2026-02-21 16:55:06 +0800

    implement device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:device_preview_plus/device_preview_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:nknu_connect/core/theme/app_theme.dart';
 import 'package:nknu_connect/features/home/presentation/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(enabled: kDebugMode, builder: (context) => MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +17,8 @@ class MyApp extends StatelessWidget {
       title: 'NKNU Connect',
       theme: AppTheme.light,
       home: HomePage(),
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
     );
   }
 }
